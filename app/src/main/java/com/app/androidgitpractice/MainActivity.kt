a5e878e7d599c9e7569a82a36f7e426df0ef137f
Vikas <dhimanv209@gmail.com>
2026-03-15 20:33:19 +0530

    login screen added

--- app/src/main/java/com/app/androidgitpractice/MainActivity.kt
+++ app/src/main/java/com/app/androidgitpractice/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.app.androidgitpractice.ui.theme.AndroidGitPracticeTheme
@@ -32,10 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly) {
+        Text(
+            text = "Hello $name Developers!",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Testing git practice",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
